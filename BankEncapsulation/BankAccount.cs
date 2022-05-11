e6a0f2cb5ea61a5705368fc3d4aba0f76b32316c
kaylafinkelstein <k.finkelstein4000@gmail.com>
2022-05-11 13:55:57 -0400

    BankEncapsulation

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        private double _balance = 0;
+
+        public void Deposit(double amount)
+        {
+            Console.WriteLine($"{amount, 0:c}");
+            _balance = amount;
+        }
+        public double GetBalance()
+        {
+            return _balance;
+        }
+    }
+}
---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BankEncapsulation
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount ba = new BankAccount();
+
+            Console.WriteLine("What is your deposit?");
+            double amountToDeposit = double.Parse(Console.ReadLine());
+
+            ba.Deposit(amountToDeposit);
+
+            double userBalance = ba.GetBalance();
+
+            Console.WriteLine($"Your balace is: {userBalance,0:c}");
+        }
+    }
+}
